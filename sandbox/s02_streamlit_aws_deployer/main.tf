174b7f11abdab95f346f1a5941e7ba85a7ed1a01
Maki <sunwood.ai.labs@gmail.com>
2024-08-10 06:50:02 +0900

    🔒 [feat] Streamlit アプリへのSSHアクセスを削除
    - セキュリティ強化のため、Streamlit アプリへのSSHアクセスを削除しました。
    - 今後は、アプリケーションのデプロイや管理にSSH接続を使用する必要はありません。

--- sandbox/s02_streamlit_aws_deployer/main.tf
+++ sandbox/s02_streamlit_aws_deployer/main.tf
@@ -62,13 +62,6 @@ resource "aws_security_group" "streamlit" {
   vpc_id      = aws_vpc.main.id
 
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
     from_port   = 8501
     to_port     = 8501
     protocol    = "tcp"
