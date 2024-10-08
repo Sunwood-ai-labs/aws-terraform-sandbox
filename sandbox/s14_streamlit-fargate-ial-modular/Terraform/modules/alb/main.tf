426803b4d2ff02781ca31efa67b7b23029d65480
Maki <sunwood.ai.labs@gmail.com>
2024-10-08 19:11:39 +0900

    🐈‍⬛ feat: ALBモジュールを作成
    - ALBモジュールを作成し、ALB、リスナー、ターゲットグループを定義しました。
    - ALBのセキュリティグループを指定し、ALBへのインバウンドトラフィックを制限しました。
    - ヘルスチェックの設定を追加して、ALBがターゲットグループ内のサービスが正常に動作しているかを確認できるようにしました。

---
+++ sandbox/s14_streamlit-fargate-ial-modular/Terraform/modules/alb/main.tf
@@ -0,0 +1,42 @@
+resource "aws_lb" "main" {
+  name               = "${var.app_name}-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [var.alb_sg_id]
+  subnets            = var.public_subnet_ids
+
+  enable_deletion_protection = false
+
+  tags = {
+    Name = "${var.app_name}-alb"
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
+}
+
+resource "aws_lb_target_group" "main" {
+  name        = "${var.app_name}-tg"
+  port        = var.container_port
+  protocol    = "HTTP"
+  vpc_id      = var.vpc_id
+  target_type = "ip"
+
+  health_check {
+    healthy_threshold   = "3"
+    interval            = "30"
+    protocol            = "HTTP"
+    matcher             = "200"
+    timeout             = "3"
+    path                = var.health_check_path
+    unhealthy_threshold = "2"
+  }
+}
