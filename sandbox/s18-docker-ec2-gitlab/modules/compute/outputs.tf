1f0211df6044e55dcf962df4467b74bc1e7e699f
Maki <sunwood.ai.labs@gmail.com>
2024-10-10 22:46:27 +0900

    🏗️ feat: modules/compute/outputs.tf作成
    - EC2インスタンスの関連する出力を定義
    - インスタンスID、パブリックIPアドレス、プライベートIPアドレス、パブリックDNS名を出力する

---
+++ sandbox/s18-docker-ec2-gitlab/modules/compute/outputs.tf
@@ -0,0 +1,19 @@
+output "instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.app_server.id
+}
+
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.app_server.public_ip
+}
+
+output "instance_private_ip" {
+  description = "Private IP address of the EC2 instance"
+  value       = aws_instance.app_server.private_ip
+}
+
+output "instance_public_dns" {
+  description = "Public DNS name of the EC2 instance"
+  value       = aws_instance.app_server.public_dns
+}
